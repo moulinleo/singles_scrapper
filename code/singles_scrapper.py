1677d15ddd36b1d41c76f8ef08e3e1d8355f22ed
moulinleo <moulinleo17@gmail.com>
2024-03-07 16:37:49 +0100

    added prints

--- code/singles_scrapper.py
+++ code/singles_scrapper.py
@@ -175,19 +175,21 @@ def add_songs_to_playlist(singles_df, SPOTIPY_USERNAME, SPOTIPY_PLAYLIST_URI, SP
     Returns:
         None
     """
-    
+    print('setting up the Spotify API authentication...')
     # Set up Spotify API authentication
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username=SPOTIPY_USERNAME, scope="playlist-modify-private",
                                                    client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, 
                                                    redirect_uri='http://localhost:8080'))
 
+    
+    print('getting existing tracks...')
     # Get the existing tracks in the playlist
     existing_tracks = get_all_playlist_tracks(sp, SPOTIPY_PLAYLIST_URI)
     
     # Extract track URIs
     existing_track_uris = set(track['track']['uri'] for track in existing_tracks)
     
-    
+    print('iterating through the DataFrame...')
     # Iterate through the DataFrame and add new songs to the playlist
     for _, row in singles_df.iterrows():
         artist = row['Artist']
@@ -292,7 +294,7 @@ min_weighted = 7.5
 base_url = 'https://www.albumoftheyear.org/releases/singles/'
 singles_df = scrape_multiple_pages(base_url,start_page, end_page, min_nb_ratings, min_rating, min_weighted)
 
-
+print('1 ****** loading env variables...')
 # Load environment variables from .env
 from dotenv import load_dotenv
 load_dotenv()
@@ -301,6 +303,7 @@ SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 SPOTIPY_USERNAME = os.getenv('SPOTIPY_USERNAME')
 SPOTIPY_PLAYLIST_URI = os.getenv('SPOTIPY_PLAYLIST_URI')
 
+print('2 ***** adding songs to playlist...')
 add_songs_to_playlist(singles_df, SPOTIPY_USERNAME, SPOTIPY_PLAYLIST_URI, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
 
 print(singles_df.head(30))
