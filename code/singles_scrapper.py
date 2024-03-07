c815b84d7ccde702eac09305a724636908ab23da
moulinleo <moulinleo17@gmail.com>
2024-03-07 19:57:47 +0100

    cached token

--- code/singles_scrapper.py
+++ code/singles_scrapper.py
@@ -194,8 +194,10 @@ def add_songs_to_playlist(singles_df, SPOTIPY_USERNAME, SPOTIPY_PLAYLIST_URI, SP
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username=SPOTIPY_USERNAME, scope="playlist-modify-private",
                                                    client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, 
                                                    redirect_uri='http://localhost:8080'))
-
-    print("Access Token:", sp.auth_manager.get_access_token())
+ 
+    access_token = sp.auth_manager.get_cached_token()
+    print("Access Token:", access_token)
+    
     # Get the existing tracks in the playlist
     print('getting existing tracks...')
     existing_tracks = get_all_playlist_tracks(sp, SPOTIPY_PLAYLIST_URI)
